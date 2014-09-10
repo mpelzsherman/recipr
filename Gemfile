3a2033135fce4e0b2efeb3a2b338cd1201ea5cdf
Michael Pelz-Sherman <mpelzsherman@rentpath.com>
2014-09-09 22:52:08 -0400

    add seed_dump gem. dump data to seed.rb file.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,10 @@ gem "twitter-bootstrap-rails"
 gem "haml"
 gem "haml-rails"
 
+group :development do
+  gem 'seed_dump'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create!([
+  {name: "Apple Pie", ingredients: "apples, crust", difficulty: "hard", directions: "bake apples, put in crust"},
+  {name: "Hamburgers", ingredients: "meat", difficulty: "very easy", directions: "make meat into patties, cook on grill"},
+  {name: "Spaghetti", ingredients: "noodles, sauce", difficulty: "easy", directions: "cook noodles, heat sauce, dump over noodles"},
+  {name: "Bean Burritos", ingredients: "tortillas, black beans, shredded cheese", difficulty: "easy", directions: "Spread a layer of beans on the tortillas, top with shredded cheese. Bake at 350 degrees for 5 minutes. Roll up and serve."},
+  {name: "Scrambled eggs", ingredients: "eggs, oiive oil", difficulty: "medium", directions: "heat olive oil in non-stick pan. beat eggs. pour into pan. remove from heat and stir until done."},
+  {name: "Stir Fry", ingredients: "oil, soy sauce, vegetables, rice, tofu", difficulty: "medium", directions: "cook rice. heat oil in wok. add vegetables and stir fry. add tofu and soy sauce. serve over rice."},
+  {name: "Frozen Pizza", ingredients: "Frozen Pizza", difficulty: "super easy", directions: "Remove pizza from box. Heat oven to 425. Bake pizza for 10 minutes."},
+  {name: "Chicken Soup", ingredients: "chicken, water, vegetables", difficulty: "hard", directions: "Put chicken in water. Boil for 1 hour. Add vegetables. Cook for another hour."},
+  {name: "Fried Potatoes", ingredients: "potatoes, oil", difficulty: "medium", directions: "heat oil in cast iron pan. cut potatoes into small pieces. fry until golden brown."},
+  {name: "Peanut Butter and Jelly Sandwiches", ingredients: "peanut butter, jelly, bread", difficulty: "medium", directions: "spread peanut butter and jelly on bread. "}
+])
